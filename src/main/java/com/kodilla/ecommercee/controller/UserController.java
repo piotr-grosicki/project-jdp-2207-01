4cd6566c2a26c2e00821b51008443f4156c5cb30
Daniel Misior <daniel.misior09@gmail.com>
2022-06-27 16:36:37 +0200

    JDP220701-05 - created UserDto and made initially implementation for UserController

--- src/main/java/com/kodilla/ecommercee/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -1,4 +1,33 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.domain.UserDto;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/users")
 public class UserController {
+
+    @PostMapping(value = "createUser", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createUser(UserDto userDto) {
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping(value = "block")
+    public ResponseEntity<UserDto> blockUser(UserDto userDto) {
+        return ResponseEntity.ok(new UserDto(
+                1L, "user1", "John",
+                "Smith", "john.smith@example.com", "address1", 1234, false));
+    }
+
+    @PutMapping(value = "key")
+    public ResponseEntity<UserDto> generateKey(UserDto userDto) {
+        return ResponseEntity.ok(new UserDto(
+                1L, "user2", "David",
+                "Plumber", "david.plumber@example.com", "address2", 4321, false));
+    }
 }
