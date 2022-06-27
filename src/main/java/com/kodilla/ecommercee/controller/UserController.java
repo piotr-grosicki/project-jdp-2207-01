7a9e6bf3292e3b20298bf4f5a7726378cbadc242
Daniel Misior <daniel.misior09@gmail.com>
2022-06-27 21:59:16 +0200

    JDP220701-5 - Added RequestBody annotation and changed required variables

--- src/main/java/com/kodilla/ecommercee/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -3,29 +3,26 @@ package com.kodilla.ecommercee.controller;
 import com.kodilla.ecommercee.domain.UserDto;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("v1/users")
 public class UserController {
 
-    @PostMapping(value = "createUser", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> createUser(UserDto userDto) {
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createUser(@RequestBody UserDto userDto) {
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping(value = "block")
-    public ResponseEntity<UserDto> blockUser(UserDto userDto) {
+    @PutMapping(value = "block/{userId}")
+    public ResponseEntity<UserDto> blockUser(@PathVariable Long userId) {
         return ResponseEntity.ok(new UserDto(
                 1L, "user1", "John",
                 "Smith", "john.smith@example.com", "address1", 1234, false));
     }
 
-    @PutMapping(value = "key")
-    public ResponseEntity<UserDto> generateKey(UserDto userDto) {
+    @PutMapping(value = "key/{userId}")
+    public ResponseEntity<UserDto> generateKey(@PathVariable Long userId) {
         return ResponseEntity.ok(new UserDto(
                 1L, "user2", "David",
                 "Plumber", "david.plumber@example.com", "address2", 4321, false));
