0448a3b58c8ab5d769ddae7d3b1589caf35c64e5
kuba jarosz <kuba.jarooosz@gmail.com>
2022-06-29 00:18:42 +0200

    User entity

---
+++ src/main/java/com/kodilla/ecommercee/entity/User.java
@@ -0,0 +1,33 @@
+package com.kodilla.ecommercee.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private long id;
+
+    @Column(name = "user_name")
+    private String name;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "address")
+    private String address;
+}
