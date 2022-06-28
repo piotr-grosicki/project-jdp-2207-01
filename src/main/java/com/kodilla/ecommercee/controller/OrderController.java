d3388eebd779fb2ef25038d4c9312351293d4b57
Dariusz Surmacz <dariuszsurmacz6@gmail.com>
2022-06-28 11:41:34 +0200

    JDP220701-4

--- src/main/java/com/kodilla/ecommercee/controller/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/controller/OrderController.java
@@ -1,4 +1,41 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.domain.OrderDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/orders")
+//@RequiredArgsConstructor
 public class OrderController {
+
+    @GetMapping
+    public List<OrderDto> getOrders() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping(value = "{orderId}")
+    public OrderDto getOrder(@PathVariable Long orderId) {
+        return new OrderDto(1L, LocalDateTime.of(2022, 6, 6, 6, 6));
+    }
+
+    @DeleteMapping
+    public void deleteOrder(Long orderId) {
+
+    }
+
+    @PutMapping
+    public OrderDto updateOrder(OrderDto orderDto) {
+        return new OrderDto(1L, LocalDateTime.of(2022, 6, 6, 6, 6));
+    }
+
+    @PostMapping
+    public void createOrder(OrderDto orderDto) {
+
+    }
+
 }
---
+++ src/main/java/com/kodilla/ecommercee/domain/OrderDto.java
@@ -0,0 +1,16 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class OrderDto {
+    private Long orderId;
+    private LocalDateTime created;
+    //user id bigint
+    //cart id bigint
+}
