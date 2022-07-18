83cccaa5c261107c93e704c3c743c8a0ec4b136c
Dariusz Surmacz <dariuszsurmacz6@gmail.com>
2022-07-18 19:55:38 +0200

    JDP220701-21 ProductController

---
+++ src/main/java/com/kodilla/ecommercee/controller/service/ProductDbService.java
@@ -0,0 +1,32 @@
+package com.kodilla.ecommercee.controller.service;
+
+import com.kodilla.ecommercee.exception.ProductNotFoundException;
+import com.kodilla.ecommercee.entity.Product;
+import com.kodilla.ecommercee.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductDbService {
+
+    private final ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProduct(final Long productId) throws ProductNotFoundException {
+        return productRepository.findById(productId).orElseThrow(ProductNotFoundException::new);
+    }
+
+    public Product createProduct(final Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(final Long productId) {
+        productRepository.deleteById(productId);
+    }
+}
