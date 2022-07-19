dd62a408c7d553a75de3e9e6d04e239704846cfb
AndrzejRajczyk <rajczyk.andrzej@gmail.com>
2022-07-19 22:36:47 +0200

    naprawilem testy junit5

--- src/test/java/com/kodilla/ecommercee/repository/CartRepositoryTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/repository/CartRepositoryTestSuite.java
@@ -5,11 +5,12 @@ import com.kodilla.ecommercee.entity.Group;
 import com.kodilla.ecommercee.entity.Order;
 import com.kodilla.ecommercee.entity.Product;
 import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
@@ -20,7 +21,7 @@ import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 
 @Transactional
 @SpringBootTest
-@RunWith(SpringRunner.class)
+
 public class CartRepositoryTestSuite {
 
     @Autowired
--- src/test/java/com/kodilla/ecommercee/repository/GroupRepositoryTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/repository/GroupRepositoryTestSuite.java
@@ -1,17 +1,18 @@
 package com.kodilla.ecommercee.repository;
 
 import com.kodilla.ecommercee.entity.Group;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+
 
 import java.util.List;
 
 import static org.junit.Assert.*;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 public class GroupRepositoryTestSuite {
 
